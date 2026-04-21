1aca7cdd9cf565ecbc4899e881129ea116f5ccc7
Nafi Sadman Khan <sadmannafi789@gmail.com>
2026-04-21 14:28:01 +0600

    testing db migrations

--- src/database/migrations/2026_04_21_063807_create_organization_documents_table.php
+++ src/database/migrations/2026_04_21_063807_create_organization_documents_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('organization_documents', function (Blueprint $table) {
             $table->id();
+            // $table->foreignId('organization_id')->constrained('organizations')->onDelete('cascade');
+            // $table->enum('document_type', ['trade_license', 'ngo_certificate', 'tax_certificate', 'other'])->nullable();
+            // $table->string('document_url');
             $table->timestamps();
         });
     }
